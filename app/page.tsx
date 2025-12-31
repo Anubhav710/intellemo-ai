89e3a234b9d8bbbffd785a034b50bf342b278791
Anubhav <anubhav@Himanshis-Laptop.local>
2025-12-31 06:55:06 +0530

    updated website

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Card from "@/components/Card";
+import CardSlider from "@/components/CardSlider";
 import Navbar from "@/components/Navbar";
 import Slider from "@/components/Slider";
 import Image from "next/image";
@@ -19,7 +19,7 @@ export default function Home() {
         </span>
         in seconds.
       </h1>
-      <Card />
+      <CardSlider />
     </div>
   );
 }
